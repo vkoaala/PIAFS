d6c14bd335512ea915c3974a6dc4c29ef1de08a9
Thuranel <jean-sebperro@outlook.com>
2017-02-09 12:30:49 -0500

    adding base project

---
+++ tests/skeleton_test.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'PHPUnit/Autoload.php';
+
+class SkeletonTest extends PHPUnit_Framework_TestCase
+{
+    public function testAssertTrueIsTrue()
+    {
+        $this->assertEquals(true, true);
+    }
+
+}
+?>
