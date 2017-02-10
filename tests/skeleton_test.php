6798d928c5ad8a13dcb1aed750981ebfd8a1368e
vkoaala <xbljustin@hotmail.com>
2017-02-10 14:04:45 -0500

    Continuous integration

--- tests/skeleton_test.php
+++ tests/skeleton_test.php
@@ -1,24 +1,26 @@
 <?php
 
+require './vendor/autoload.php';
 include_once 'src/plugins/skeleton_deprecated/calculator.class.php';
+
 
 class SkeletonTest extends PHPUnit_Framework_TestCase
 {
-    public function testAssertTrueIsTrue()
-    {
-        $this->assertEquals(true, true);
-    }
+   public function testAssertTrueIsTrue()
+   {
+       $this->assertEquals(true, true);
+   }
 
-    public function testCalculatorAddCanAddNumbers(){
-      $calculator = new Calculator();
-      $result = $calculator->add(1, 1);
-      $this->assertEquals($result, 2);
-    }
+   public function testCalculatorAddCanAddNumbers(){
+     $calculator = new Calculator();
+     $result = $calculator->add(1, 1);
+     $this->assertEquals($result, 2);
+   }
 
-    public function testCalculatorCanIncrement(){
-      $calculator = new Calculator();
-      $result = $calculator->increment(1);
-      $this->assertEquals($result, 2);
-    }
+   public function testCalculatorCanIncrement(){
+     $calculator = new Calculator();
+     $result = $calculator->increment(1);
+     $this->assertEquals($result, 2);
+   }
 }
 ?>
