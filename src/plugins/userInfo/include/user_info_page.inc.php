822e79e38e1148599366877a374449c56e5c5c70
Thuranel <jean-sebperro@outlook.com>
2017-02-15 15:43:34 -0500

    fix if there are no user info yet

--- src/plugins/userInfo/include/user_info_page.inc.php
+++ src/plugins/userInfo/include/user_info_page.inc.php
@@ -10,16 +10,20 @@ $query = '
 
 $result = pwg_db_fetch_assoc(pwg_query($query));
 
-
 $template->assign(array(
   // this is useful when having big blocks of text which must be translated
   // prefer separated HTML files over big lang.php files
   'INTRO_CONTENT' => load_language('intro.html', USER_INFO_PATH, array('return'=>true)),
   'USER_INFO_PATH' => USER_INFO_PATH,
-  'USER_INFO_ABS_PATH' => realpath(USER_INFO_PATH).'/',
+  'USER_INFO_ABS_PATH' => realpath(USER_INFO_PATH).'/'
+  ));
+
+if(!empty($result)){
+  $template->assign(array(
   'USER_FNAME' => array_values($result)[1],
   'USER_LNAME' => array_values($result)[2]
   ));
+}
 
 $template->set_filename('user_info_page', realpath(USER_INFO_PATH . 'template/user_info_page.tpl'));
 $template->assign_var_from_handle('CONTENT', 'user_info_page');
