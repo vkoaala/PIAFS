1dc169bdfa805b2ab84d946e91892b39cfcbdc16
vkoaala <xbljustin@hotmail.com>
2017-02-22 10:42:11 -0500

    Updated .gitignore

--- src/local/config/config.inc.bak.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* The file does not exist until some information is entered 
-below. Once information is entered and saved, the file will be created. */
-
-
-$conf['upload_form_all_types'] = true;
-
-?>
